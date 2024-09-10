79b376ea7b00813d17f80bab7783edb725256276
cyteon <cyteon>
2024-09-10 14:44:39 +0200

    newlines

--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -1,3 +1,4 @@
+use core::fmt;
 use volatile::Volatile;
 
 #[allow(dead_code)]
@@ -88,11 +89,41 @@ impl Writer {
     }
 
     fn new_line(&mut self) {
-        // Todo
+        for row in 1..BUFFER_HEIGHT {
+            for col in 0..BUFFER_WIDTH {
+                let char = self.buffer.chars[row][col].read();
+
+                self.buffer.chars[row - 1][col].write(char);
+            }
+        }
+
+        self.clear_row(BUFFER_HEIGHT - 1);
+
+        self.column_position = 0;
+    }
+
+    fn clear_row(&mut self, row: usize) {
+        let blank = ScreenChar {
+            ascii_character: b' ',
+            color_code: self.color_code,
+        };
+
+        for col in 0..BUFFER_WIDTH {
+            self.buffer.chars[row][col].write(blank);
+        }
+    }
+}
+
+impl fmt::Write for Writer {
+    fn write_str(&mut self, s: &str) -> fmt::Result {
+        self.write_string(s);
+        Ok(())
     }
 }
 
 pub fn print_something() {
+    use core::fmt::Write;
+
     let mut writer = Writer {
         column_position: 0,
         color_code: ColorCode::new(Color::White, Color::Black),
@@ -101,5 +132,7 @@ pub fn print_something() {
 
     writer.write_string("Hello");
     writer.write_byte(b',');
-    writer.write_string(" world!");
+    writer.write_string(" world!\n");
+
+    write!(writer, "1 = {}, 2 = {}, 2.5 = {}", 1, 2, 2.5).unwrap()
 }
