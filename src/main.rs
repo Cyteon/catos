bc4c8310e328691b22354621800fc947bf3949b5
cyteon <129582290+Cyteon@users.noreply.github.com>
2024-09-16 19:06:46 +0200

    more paging stuff

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,16 @@
 #![no_main]
 
 use bootloader::{entry_point, BootInfo};
-use cat_os::{clear, memory::active_level_4_table, println};
+use cat_os::{
+    clear,
+    memory::{self, translate_addr},
+    println,
+};
 use core::panic::PanicInfo;
-use x86_64::{structures::paging::PageTable, VirtAddr};
+use x86_64::{
+    structures::paging::{PageTable, Translate},
+    VirtAddr,
+};
 
 // Set entry point
 entry_point!(kernel_main);
@@ -25,24 +32,22 @@ fn kernel_main(boot_info: &'static BootInfo) -> ! {
     cat_os::init();
 
     let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
-    let l4_table = unsafe { active_level_4_table(phys_mem_offset) };
 
-    for (i, entry) in l4_table.iter().enumerate() {
-        if !entry.is_unused() {
-            println!("L4 Entry {}: {:?}", i, entry);
+    // Initialize mapper
+    let mapper = unsafe { memory::init(phys_mem_offset) };
 
-            let phys = entry.frame().unwrap().start_address();
-            let virt = phys.as_u64() + boot_info.physical_memory_offset;
-            let ptr = VirtAddr::new(virt).as_mut_ptr();
+    println!("Testing Memory Pages");
+    let addresses = [
+        // VGA BUFFER
+        0xb8000,
+        boot_info.physical_memory_offset,
+    ];
 
-            let l3_table: &PageTable = unsafe { &*ptr };
+    for &addr in &addresses {
+        let virt = VirtAddr::new(addr);
+        let phys = mapper.translate_addr(virt);
 
-            for (i, entry) in l3_table.iter().enumerate() {
-                if !entry.is_unused() {
-                    println!("L3 Entry {}: {:?}", i, entry);
-                }
-            }
-        }
+        println!("{:?} -> {:?}", virt, phys)
     }
 
     loop {
--- src/memory.rs
+++ src/memory.rs
@@ -1,6 +1,15 @@
-use x86_64::{structures::paging::PageTable, VirtAddr};
+use x86_64::{
+    structures::paging::{OffsetPageTable, PageTable},
+    PhysAddr, VirtAddr,
+};
 
-pub unsafe fn active_level_4_table(physical_memory_offset: VirtAddr) -> &'static mut PageTable {
+pub unsafe fn init(physical_memory_offset: VirtAddr) -> OffsetPageTable<'static> {
+    let level_4_table = active_level_4_table(physical_memory_offset);
+
+    OffsetPageTable::new(level_4_table, physical_memory_offset)
+}
+
+unsafe fn active_level_4_table(physical_memory_offset: VirtAddr) -> &'static mut PageTable {
     use x86_64::registers::control::Cr3;
 
     let (level_4_page_table, _) = Cr3::read();
@@ -10,4 +19,41 @@ pub unsafe fn active_level_4_table(physical_memory_offset: VirtAddr) -> &'static
     let page_table_ptr: *mut PageTable = virt.as_mut_ptr();
 
     &mut *page_table_ptr
+}
+
+/// Translates the given virt addr to the mapped physical addr, or none if not mapped
+pub unsafe fn translate_addr(addr: VirtAddr, physical_memory_offset: VirtAddr) -> Option<PhysAddr> {
+    translate_addr_inner(addr, physical_memory_offset)
+}
+
+fn translate_addr_inner(addr: VirtAddr, physical_memory_offset: VirtAddr) -> Option<PhysAddr> {
+    use x86_64::registers::control::Cr3;
+    use x86_64::structures::paging::page_table::FrameError;
+
+    let (level_4_table_frame, _) = Cr3::read();
+
+    let table_indexes = [
+        addr.p4_index(),
+        addr.p3_index(),
+        addr.p2_index(),
+        addr.p1_index(),
+    ];
+
+    let mut frame = level_4_table_frame;
+
+    for &index in &table_indexes {
+        let virt = physical_memory_offset + frame.start_address().as_u64();
+        let table_ptr: *const PageTable = virt.as_ptr();
+        let table = unsafe { &*table_ptr };
+
+        let entry = &table[index];
+
+        frame = match entry.frame() {
+            Ok(frame) => frame,
+            Err(FrameError::FrameNotPresent) => return None,
+            Err(FrameError::HugeFrame) => panic!("Huge pages not supported"),
+        };
+    }
+
+    Some(frame.start_address() + u64::from(addr.page_offset()))
 }
